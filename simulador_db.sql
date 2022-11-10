68aacac430312fdc3f93eb445a0a49e92276f7a7
pedro342 <pedrito__0812@live.com>
2022-11-10 12:35:12 -0300

    ProyectoMYSQL

---
+++ simulador_db.sql
@@ -0,0 +1,47 @@
+INSERT INTO animales_terrestres (tipo, nombre_cientifico, color, tamaño, cantidad_patas) VALUES ();
+INSERT INTO animales_terrestres (tipo, nombre_cientifico, color, tamaño, cantidad_patas) VALUES ();
+INSERT INTO animales_terrestres (tipo, nombre_cientifico, color, tamaño, cantidad_patas) VALUES ();
+INSERT INTO animales_terrestres (tipo, nombre_cientifico, color, tamaño, cantidad_patas) VALUES ();
+INSERT INTO animales_terrestres (tipo, nombre_cientifico, color, tamaño, cantidad_patas) VALUES ();
+INSERT INTO animales_terrestres (tipo, nombre_cientifico, color, tamaño, cantidad_patas) VALUES ();
+INSERT INTO animales_terrestres (tipo, nombre_cientifico, color, tamaño, cantidad_patas) VALUES ();
+
+INSERT INTO animales_aereos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_aereos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_aereos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_aereos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_aereos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_aereos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_aereos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_aereos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_aereos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+
+INSERT INTO animales_acuaticos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_acuaticos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_acuaticos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_acuaticos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_acuaticos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_acuaticos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_acuaticos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_acuaticos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO animales_acuaticos (tipo, nombre_cientifico, color, tamaño) VALUES ();
+
+INSERT INTO arboles (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO arboles (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO arboles (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO arboles (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO arboles (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO arboles (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO arboles (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO arboles (tipo, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO arboles (tipo, nombre_cientifico, color, tamaño) VALUES ();
+
+INSERT INTO flores (tipo, petalos, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO flores (tipo, petalos, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO flores (tipo, petalos, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO flores (tipo, petalos, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO flores (tipo, petalos, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO flores (tipo, petalos, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO flores (tipo, petalos, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO flores (tipo, petalos, nombre_cientifico, color, tamaño) VALUES ();
+INSERT INTO flores (tipo, petalos, nombre_cientifico, color, tamaño) VALUES ();
